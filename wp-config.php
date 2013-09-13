a74a51a3de96363ee3c9e76d50acbd3c9376784b
lokesh <lokesh@s-creative>
2013-09-13 15:35:38 +0530

    Signed-off-by: lokesh <lokesh@s-creative>

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'bronze');
+define('DB_NAME', 'shill_rihams');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'shill_rihams');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', 'rihams123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
